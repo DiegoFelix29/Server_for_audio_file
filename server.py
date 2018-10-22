4d3c4856cafb3a21a8e0607f59644cc57d4db051
Diego F <diegoalonsofelix05@gmail.com>
2018-10-22 15:51:42 -0500

    actua server

--- server.py
+++ server.py
@@ -2,17 +2,12 @@ import main_audio
 from flask import Flask
 from flask import Response, jsonify, request, redirect, url_for
 from flask import render_template, flash, redirect, url_for
-#serving files that were uploaded
 from flask import send_from_directory
-
-#persistent storage in dictionary
 import shelve
 import socket
 import os
-#from config import Config
 from werkzeug.utils import secure_filename
 
-#mac-specific relative path to the script's location
 UPLOAD_FOLDER = './audio'
 ALLOWED_EXTENSIONS = set(['wav', 'ogg', 'mp3'])
 
@@ -23,11 +18,11 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
     print 'upload file'
     try:
-        ### .audio
+       
         os.stat(app.config['UPLOAD_FOLDER'])
     except:        
         os.mkdir(app.config['UPLOAD_FOLDER'])
@@ -42,11 +37,7 @@ def upload_file():
             paths=os.path.join(app.config['UPLOAD_FOLDER'])
             filenames = secure_filename(file.filename)
             main_audio.audio(file,paths,filenames)
-                
 
-#    return send_from_directory(app.config['UPLOAD_FOLDER'],
-#    						   "_1")
 if __name__ == '__main__':
 
-    print(socket.gethostbyname(socket.gethostname()))
     app.run(host='0.0.0.0')
